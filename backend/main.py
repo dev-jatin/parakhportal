9f87f8964f1f095305c26306fe016b5e8c266c9f
Jatin Gupta <76555005+dev-jatin@users.noreply.github.com>
2023-12-20 18:17:02 +0530

    push code

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,20 @@
 
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
+from PyPDF2 import PdfReader
+
+
+from tempfile import NamedTemporaryFile
+
+
+
+from openai import OpenAI
+
+
+client = OpenAI(api_key="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")
+
+
 
 def determine_blooms_level(question):
    
@@ -40,8 +54,6 @@ def determine_blooms_level(question):
 app = FastAPI()
 
 
-app = FastAPI()
-
 origins = ["*"]  
 
 app.add_middleware(
@@ -50,7 +62,35 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+
 )
+
+
+def extract_text_from_pdf(pdf_file):
+    text = ""
+    reader = PdfReader(pdf_file)
+    number_of_pages = len(reader.pages)
+    for page_number in range(number_of_pages):
+        page = reader.pages[page_number]
+        text = text + page.extract_text()     
+
+    return text
+
+@app.post("/extract_text")
+async def extract_text(file: UploadFile = File(...)):
+    temp_pdf_path = None
+    try:
+        with NamedTemporaryFile(delete=False) as temp_pdf:
+            temp_pdf.write(file.file.read())
+            temp_pdf_path = temp_pdf.name
+            text = extract_text_from_pdf(temp_pdf)
+            return JSONResponse(content={'text': text}, status_code=200)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    finally:
+        if temp_pdf_path:
+            # Cleanup temporary file
+            temp_pdf.close()
 
 @app.get("/get_categories/{question}")
 def get_categories(question: str):
@@ -59,3 +99,16 @@ def get_categories(question: str):
 
     return {"category": category}
 
+
+
+
+@app.post("/generate_question/")
+def get_categories(text: str):
+
+    response = client.completions.create(
+        model="gpt-3.5-turbo-instruct",
+        prompt="Generate the multiple choice question from  this topic:\n {text}\nOptions"
+    )
+
+
+    return JSONResponse(content={'data:': response.choices[0].text}, status_code=200)
